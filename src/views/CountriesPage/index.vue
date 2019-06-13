32c2d43873696aca4449abcb17bbaa79faed95c6
Kami-san <1.dankon.1@gmail.com>
2019-06-13 23:42:25 +0300

    Страница с выводом списка всех путешествий

--- src/views/CountriesPage/index.vue
+++ src/views/CountriesPage/index.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="tables"></div>
+  <v-layout
+    justify-center
+    align-center
+    fill-height
+  >
+    <v-flex xs6 my-5>
+      <v-list two-line subheader>
+        <v-subheader inset>Все путешествия</v-subheader>
+
+        <v-list-tile
+          v-for="(adventure, index) in adventures"
+          :key="adventure.id"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ index + 1 }}. 
+              {{ adventure.country.name }} -
+              {{ adventure.city.name }}
+            </v-list-tile-title>
+            <v-list-tile-sub-title>
+              {{ adventure.date }}
+              <v-divider />
+            </v-list-tile-sub-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
