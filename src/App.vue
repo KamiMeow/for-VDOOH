4c12e8f38e60ea8ce11553b944fb23de8394bbed
Kami-san <1.dankon.1@gmail.com>
2019-06-13 23:41:24 +0300

    Описание общей конструкции приложения

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,32 @@
 <template>
   <v-app>
-    <v-content>
-      <router-view />
-    </v-content>
+    <v-layout fill-height>
+      <v-flex shrink>
+        <the-menu-app />
+      </v-flex>
+
+      <v-flex grow>
+        <router-view />
+      </v-flex>
+    </v-layout>
   </v-app>
 </template>
 
 <script>
+import TheMenuApp from './components/TheMenuApp';
+
 export default {
   name: 'App',
+  created() {
+    this.initAdventures();
+  },
+  components: {
+    TheMenuApp,
+  },
+  methods: {
+    async initAdventures() {
+      this.$store.dispatch('initAdventures', (await this.Net.get('adventures')));
+    },
+  },
 };
 </script>
