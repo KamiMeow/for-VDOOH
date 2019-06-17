8510df90af009937c86bd8764d021ae6854dd4ef
Kami-san <1.dankon.1@gmail.com>
2019-06-17 19:45:18 +0300

    Изменил стиль списка стран

--- src/views/CountriesPage/index.vue
+++ src/views/CountriesPage/index.vue
@@ -5,26 +5,37 @@
     fill-height
   >
     <v-flex xs6 my-5>
-      <v-list two-line subheader>
-        <v-subheader inset>Все путешествия</v-subheader>
+      <v-card>
+        <v-card-title class="headline px-4">
+          Все путешествия
+        </v-card-title>
 
-        <v-list-tile
-          v-for="(adventure, index) in adventures"
-          :key="adventure.id"
-        >
-          <v-list-tile-content>
-            <v-list-tile-title>
-              {{ index + 1 }}. 
-              {{ adventure.country.name }} -
-              {{ adventure.city.name }}
-            </v-list-tile-title>
-            <v-list-tile-sub-title>
-              {{ adventure.date }}
-              <v-divider />
-            </v-list-tile-sub-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
+        <v-divider></v-divider>
+
+        <v-card-text>
+          <v-list two-line subheader>
+            <v-list-tile
+              v-for="(adventure, index) in adventures"
+              :key="adventure.id"
+            >
+              <v-list-tile-content>
+                <v-list-tile-title class="subheading">
+                  {{ index + 1 }}. 
+                  <span class="font-weight-bold">
+                    {{ adventure.country.name }}
+                  </span> - 
+                  г. {{ adventure.city.name }}
+                </v-list-tile-title>
+
+                <v-list-tile-sub-title>
+                  <div class="py-1 px-3">{{ adventure.date }}</div>
+                  <v-divider />
+                </v-list-tile-sub-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
