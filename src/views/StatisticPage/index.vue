5e04a2432002e97b1d687a708a40174fecdadae8
Kami-san <1.dankon.1@gmail.com>
2019-06-17 19:43:59 +0300

    Страница статистики

--- src/views/StatisticPage/index.vue
+++ src/views/StatisticPage/index.vue
@@ -1,10 +1,66 @@
 <template>
-  <div class="map"></div>
+  <v-layout
+    justify-center
+    align-center
+    fill-height
+  >
+    <v-flex xs5>
+      <v-card elevation-0>
+        <v-card-text>
+          <line-chart :chart-data="chartData"></line-chart>
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
+import LineChart from './lineChart.js';
+
+const years = {
+  more: 0,
+  less: 0,
+};
+
+const chartData = {
+  labels: [],
+  datasets: [
+    {
+      label: 'Количество путешествий в год',
+      backgroundColor: '#0082ff',
+      data: [],
+    },
+  ],
+};
+
 export default {
   name: 'StatisticPage',
+  components: {
+    LineChart,
+  },
+  async created() {
+    this.adventures.reverse().forEach(adveture => {
+      const year = this.getYear(adveture.date);
+
+      if (!~this.chartData.labels.indexOf(year)) {
+        this.chartData.labels.push(year);
+      }
+    });
+
+    this.chartData.labels.forEach((label, index) => {
+      const adventures = this.adventures.filter(a => this.getYear(a.date) === label);
+      this.chartData.datasets[0].data[index] = adventures.length;
+    });
+  },
+  data: () => ({
+    chartData,
+    years,
+  }),
+  methods: {
+    getYear(date) {
+      return date.split('-')[0];
+    },
+  },
 };
 </script>
 
