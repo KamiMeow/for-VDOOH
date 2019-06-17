a94d49fe7ebf3edc7d23bef0a5c6c224daec7729
Kami-san <1.dankon.1@gmail.com>
2019-06-17 19:44:39 +0300

    Страница карты

--- src/views/MapPage/index.vue
+++ src/views/MapPage/index.vue
@@ -1,40 +1,119 @@
 <template>
-  <yandex-map
-    ref="yMap"
-    zoom="4"
-    :coords="coordinates"
-    :controls="[]"
-    style="width: 100%; height: 100vh;"
+  <v-layout
+    fill-height
+    wrap
   >
-    <!-- <ymap-marker
-      v-for="(marker, index) in mapData.markers"
-      :key="index + 1"
-      :marker-id="index + 1"
-      :marker-fill="{ color: styles[marker.task_type_cd].fill, opacity: 1 }"
-      :marker-stroke="{ color: styles[marker.task_type_cd].stroke, width: 4}"
-      :coords="marker.coordinates"
-      :hint-content="marker.task_status_cd"
-      marker-type="circle"
-      circle-radius="50"
-      :callbacks="{ click: () => {
-        createWay(marker.machine_id);
-      }}"
-    /> -->
-  </yandex-map>
+    <loading-progress
+      v-if="loading"
+      label="Загрузка карты"
+    />
+
+    <yandex-map
+      v-show="!loading"
+      ref="yMap"
+      zoom="4"
+      :coords="coordinates"
+      :controls="[]"
+      style="width: 100%; height: 100vh;"
+      @map-was-initialized="initMap"
+    >
+      <!-- <ymap-marker
+        v-for="(marker, index) in mapData.markers"
+        :key="index + 1"
+        :marker-id="index + 1"
+        :marker-fill="{ color: styles[marker.task_type_cd].fill, opacity: 1 }"
+        :marker-stroke="{ color: styles[marker.task_type_cd].stroke, width: 4}"
+        :coords="marker.coordinates"
+        :hint-content="marker.task_status_cd"
+        marker-type="circle"
+        circle-radius="50"
+        :callbacks="{ click: () => {
+          createWay(marker.machine_id);
+        }}"
+      /> -->
+    </yandex-map>
+  </v-layout>
 </template>
 
 <script>
+import LoadingProgress from '@/components/LoadingProgress';
+
+const colors = ['#42A5F5', '#1976D2', '#0D47A1', '#5C6BC0', '#3949AB', '#283593', '#1A237E'];
+
 export default {
   name: "MapPage",
+  components: {
+    LoadingProgress,
+  },
   data: () => ({
+    colors,
+    color: 0,
+
+    loading: true,
     coordinates: [54.62896654088406, 39.731893822753904],
   }),
+  methods: {
+    initMap() {
+      this.loading = false;
+
+      const { yMap } = this.$refs;
+      let lastCoord;
+
+      this.adventures.forEach(adventure => {
+        ymaps.geocode(adventure.city.name, {
+          results: 1
+        })
+          .then(res => {
+            var firstGeoObject = res.geoObjects.get(0);
+
+            if (!!firstGeoObject.geometry) {
+              const coord = firstGeoObject.geometry.getCoordinates();
+
+              if (!!lastCoord) {
+                this.createLine(yMap, coord, lastCoord);
+              }
+              lastCoord = coord;
+
+              firstGeoObject.properties.set('iconCaption', firstGeoObject.getAddressLine());
+              firstGeoObject.properties.set('balloonContentBody',
+                `<span class="subheading font-weight-bold">${adventure.country.name}</span>` +
+                ` - <span>${adventure.city.name}</span>` +
+                `<div class="body font-italic">${adventure.date}</div>`);
+              firstGeoObject.options.set('preset', 'islands#nightCircleIcon');
+
+
+              yMap.myMap.geoObjects.add(firstGeoObject);
+            }
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      });
+    },
+    createLine(yMap, coord, lastCoord) {
+      if (coord !== lastCoord) {
+        const adventureLine = new ymaps.Polyline([coord, lastCoord], {
+          balloonContent: 'Линия путешествия',
+        },
+        {
+          balloonCloseButton: false,
+          strokeColor: this.colors[this.color],
+          strokeWidth: 4,
+          strokeOpacity: 0.8,
+        });
+
+        if (this.color === colors.length) this.color = 0;
+        else this.color++;
+
+        yMap.myMap.geoObjects.add(adventureLine);
+      }
+    },
+  },
 };
 </script>
 
 <style>
-.ymaps-2-1-74-map-copyrights-promo,
-.ymaps-2-1-74-copyright {
+.ymaps-2-1-74-map-copyrights-promo {
   display: none !important;
 }
 .ymaps-2-1-74-ground-pane {
